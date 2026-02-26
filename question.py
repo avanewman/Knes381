4deee56b4018eed2a942d935b875b4847c1c4a3f
Ava Newman <newmanava040@gmail.com>
2026-02-26 12:23:39 -0700

    I began learning python today with 2 new files

---
+++ question.py
@@ -0,0 +1,14 @@
+# lines that start with a number sign / pound key
+
+# are comment lines that won't be read by the program 
+
+"""
+3 quotess start and 
+stop a multiline comment 
+"""
+
+print ('Hello there, what is your name?')
+my_name=input()
+print ('Nice to meet you ', my_name)
+
+
---
+++ rocket.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+def printRocket():
+	print(
+		"""
+
+
+			-
+		     /     \\
+		     |  -  |
+		     |     |
+		    /       \\
+		   /         \\
+
+		""" )
+printRocket()
+
+delay = 300
+for i in range (60):
+	print()
+	sleep(delay/1000)
+	delay = delay *0.9
+
